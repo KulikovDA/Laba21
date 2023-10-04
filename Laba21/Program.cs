0ba7a2813352064d89b10a53cdf0601cb5c962a6
Arion <Arion@DESKTOP-9BU80RS>
2023-10-04 15:04:53 +0200

    Добавьте файлы проекта.

---
+++ Laba21/Program.cs
@@ -0,0 +1,6 @@
+﻿//высокий уровень 25
+Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine((x >= -1 && y <= x && y < 0) || (x <=1 && y <= -x && y <0));
